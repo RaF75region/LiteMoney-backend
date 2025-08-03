00ae26905b8de18f566ee9666c14d595ebf5d86d
akolobov <akolobov@softproduct.pro>
2025-08-02 22:05:56 -0300

    Add auth

--- LiteMoney.Api/Program.cs
+++ LiteMoney.Api/Program.cs
@@ -1,16 +1,17 @@
+using System.Security.Claims;
 using LiteMoney.Infrastructure.Data;
 using LiteMoney.Application.Interfaces;
 using LiteMoney.Application.Services;
 using LiteMoney.Infrastructure.Repositories;
 using LiteMoney.Domain.Models;
 using LiteMoney.Infrastructure.GroupMaps;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<LiteMoneyDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -19,14 +20,22 @@ builder.Services.AddScoped<IAccountService, AccountService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ITransactionService, TransactionService>();
 builder.Services.AddIdentityCore<ApplicationUser>()
+    .AddRoles<IdentityRole>()
+    // .AddClaimsPrincipalFactory<ApplicationUser>()
     .AddEntityFrameworkStores<LiteMoneyDbContext>();
 builder.Services.AddIdentityApiEndpoints<ApplicationUser>();
 builder.Services.AddAuthorization();
 builder.Services.AddEndpointGroups(typeof(AccountGroupMap).Assembly);
+builder.Services.AddAuthentication()
+    .AddBearerToken();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin", policy => policy.RequireRole("ADMIN"));
+    options.AddPolicy("CanDelete", policy => policy.RequireClaim("permission", "can:delete"));
+});
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -34,9 +43,15 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapIdentityApi<ApplicationUser>();
-
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapEndpointGroups();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 
 app.Run();
 
